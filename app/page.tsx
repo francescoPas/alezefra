e8d6600dfe6bd3c38dd76a598a49b87388fdb285
Fra <pascalifrancesco.3b@gmail.com>
2026-04-28 14:25:03 +0200

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+      <h1 className="text-9xl font-bold text-white pt-20">ALEZ & FRA</h1>
+    </div>
+  );
+}
