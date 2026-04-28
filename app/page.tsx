b4674a6acc04dd0c5bc15dae944f155e839ff130
Fra <pascalifrancesco.3b@gmail.com>
2026-04-28 14:44:19 +0200

    Sottotitolo di prova

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black">
       <h1 className="text-9xl font-bold text-white pt-20">ALEZ & FRA</h1>
+      <p className="text-white text-lg mt-10">Welcome to our website!</p>
     </div>
   );
 }
